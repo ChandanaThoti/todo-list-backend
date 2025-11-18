6bb85e1364029462aaa510c2ffaa0664caa719bb
ChandanaThoti <chandana.thoti@everest.engineering>
2025-11-18 14:10:28 +0530

    Update task type with status,priority as string

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,20 +1,8 @@
 export type Task = {
-  Id: string;
-  Name: string;
-  Description: string;
-  Status: Status;
-  Priority: Priority;
-  Deadline: string;
+  id: string;
+  name: string;
+  description: string;
+  status: string;
+  priority: string;
+  deadline: string;
 };
-
-export enum Status {
-  InProgress = "In Progress",
-  Pending = "Pending",
-  Completed = "Completed",
-}
-
-export enum Priority {
-  High = "High priority",
-  Medium = "Medium priority",
-  Low = "Low priority",
-}
