353b6c3648f513d7120c25a18331ef979e2801f7
ChandanaThoti <chandana.thoti@everest.engineering>
2025-11-18 17:58:17 +0530

    Update route for patch updateTask

--- src/routes/taskRoute/taskRoute.ts
+++ src/routes/taskRoute/taskRoute.ts
@@ -2,11 +2,13 @@ import express from "express";
 import {
   addTask,
   getTasks,
+  updateTask,
 } from "../../controller/taskController/taskController";
 
 const taskRouter = express.Router();
 
 taskRouter.post("/tasks", addTask);
 taskRouter.get("/tasks", getTasks);
+taskRouter.patch("/tasks", updateTask);
 
 export default taskRouter;
